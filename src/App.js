2e1dc552689977391d98271ab60b41880f8fb699
Арслан Арифуллин <arifarsrad@gmail.com>
2021-08-27 12:55:10 +0300

    Adding reducers and redux

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar"
 import MyPosts from "./components/posts/MyPosts";
 import Dialogs from "./components/dialogs/Dialogs";
-import {BrowserRouter, Router, Route} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import Friends from "./components/friends/Friends";
 
 const App = (props) => {
@@ -14,9 +14,9 @@ const App = (props) => {
           <Header />
             <NavBar />
             <div class='app-wrapper-content'>
-                <Route path='/posts' render={ () => <MyPosts state={props.state.postsPage}
+                <Route path='/posts' render={ () => <MyPosts state={props.state.profileReducer}
                                                              dispatch={props.dispatch}/>} />
-                <Route path='/dialogs' render={ () => <Dialogs state={props.state.messagesPage}
+                <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsReducer}
                                                                dispatch={props.dispatch}
                                                                store={props.store}/>} />
                 <Route path='/friends' render={ () => <Friends state={props.state.friendsPage} />} />
