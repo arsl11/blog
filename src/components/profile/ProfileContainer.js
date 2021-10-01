a277db5bfdabe5553870401ce0b37b7da36e8542
Арслан Арифуллин <arifarsrad@gmail.com>
2021-10-01 14:07:28 +0300

    Change ProfileComponent to pure component, changing profile-reducer.js with TS

--- src/components/profile/ProfileContainer.js
+++ src/components/profile/ProfileContainer.js
@@ -6,7 +6,7 @@ import {withRouter} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
 
-class ProfileContainer extends React.Component {
+class ProfileContainer extends React.PureComponent {
 
     componentDidMount() {
         let userId = this.props.match.params.userId;
---
+++ src/redux/profile-reducer.tsx
@@ -0,0 +1,87 @@
+import {profileAPI} from "../api/api";
+
+const ADD_POST = 'ADD-POST';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_PROFILE_STATUS = 'SET_PROFILE_STATUS'
+
+
+type PostType = {
+    id: number,
+    message: string,
+    likesCount: number
+}
+
+type InitialStateType = {
+    posts: Array<PostType>
+    profile: any
+    profileStatus: string
+}
+
+let initialState: InitialStateType = {
+    posts: [{id: 1, message:'hello world!', likesCount:0}],
+    profile: null,
+    profileStatus: '',
+}
+
+const profileReducer = (state = initialState, action: any): InitialStateType => {
+    switch (action.type){
+        case ADD_POST: {
+            let newPost = {
+                id: 3,
+                message: action.newPostBody,
+                likesCount: 0
+            };
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+            };
+        }
+        case SET_USER_PROFILE:
+            return {...state, profile: action.profile}
+        case SET_PROFILE_STATUS:
+            return {...state, profileStatus: action.profileStatus}
+        default:
+            return state;
+    }
+}
+
+type addPostActionType = {
+    type: typeof ADD_POST,
+    newPostBody: string
+}
+export const addPostActionCreator = (newPostBody: string) : addPostActionType => ({type: ADD_POST, newPostBody})
+
+type setUserProfileType = {
+    type: typeof SET_USER_PROFILE,
+    profile: any
+}
+export const setUserProfile = (profile: any): setUserProfileType => ({type: SET_USER_PROFILE, profile})
+
+type setProfileStatusType = {
+    type: typeof SET_PROFILE_STATUS,
+    profileStatus: string
+}
+export const setProfileStatus = (profileStatus: string): setProfileStatusType => ({type: SET_PROFILE_STATUS, profileStatus})
+
+export const requestProfile = (userId: number) => (dispatch: any) => {
+    profileAPI.getProfile(userId).then((data: any) => {
+        dispatch(setUserProfile(data));
+    });
+}
+
+export const requestProfileStatus = (userId: number) => (dispatch: any) => {
+    profileAPI.getProfileStatus(userId).then((response: any) => {
+       dispatch(setProfileStatus(response.data));
+    });
+}
+
+export const updateStatus = (status: string) => (dispatch: any) => {
+    profileAPI.updateStatus(status).then((response: any) => {
+        if (response.data.resultCode === 0){
+            dispatch(setProfileStatus(status))
+        }
+    })
+}
+
+
+export default profileReducer;
